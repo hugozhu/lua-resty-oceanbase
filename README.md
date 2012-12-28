a4bdf2f290077f23c91994f1a35fd59da4bce73a
hugozhu <hugozhu@gmail.com>
2012-12-28 15:41:13 +0800

    init checking in

--- README.md
+++ README.md
@@ -1,2 +1,105 @@
-lua-resty-oceanbase
-===================
+Name
+====
+
+lua-resty-oceanbase - Lua OceanBase client driver for ngx_lua based on the cosocket API
+
+Status
+======
+
+This library is considered not production ready.
+
+Description
+===========
+
+This Lua library is a OceanBase client driver for the ngx_lua nginx module:
+
+http://wiki.nginx.org/HttpLuaModule
+
+This Lua library takes advantage of ngx_lua's cosocket API, which ensures
+100% nonblocking behavior.
+
+Note that at least [ngx_lua 0.5.7](https://github.com/chaoslawful/lua-nginx-module/tags) or [ngx_openresty 1.2.1.7](http://openresty.org/#Download) is required.
+
+Also, the [bit library](http://bitop.luajit.org/) is also required. If you're using LuaJIT 2.0 with ngx_lua, then the `bit` library is already available by default.
+
+Synopsis
+========
+
+    lua_package_path "/path/to/lua-resty-oceanbase/lib/?.lua;;";
+
+    server {
+        location /test {
+            content_by_lua '
+                local oceanbase = require "resty.oceanbase"
+                local db, err = oceanbase:new()
+
+                local ok, err, errno, sqlstate = db:connect{
+                    host = "127.0.0.1",
+                    port = 5432,
+                }
+
+                local function callback(row)
+                    ngx.say(table.concat(row, ", "))
+                end    
+
+                if ok then
+                    local sql = [[select * from test limit 100 offset 0]]
+                    db:query(sql, callback)
+                end
+
+                db:close()                
+            ';
+        }
+    }
+
+
+Limitations
+===========
+
+* This library cannot be used in code contexts like set_by_lua*, log_by_lua*, and
+header_filter_by_lua* where the ngx_lua cosocket API is not available.
+* The `resty.oceanbase` object instance cannot be stored in a Lua variable at the Lua module level,
+because it will then be shared by all the concurrent requests handled by the same nginx
+ worker process (see
+http://wiki.nginx.org/HttpLuaModule#Data_Sharing_within_an_Nginx_Worker ) and
+result in bad race conditions when concurrent requests are trying to use the same `resty.mysql` instance.
+You should always initiate `resty.mysql` objects in function local
+variables or in the `ngx.ctx` table. These places all have their own data copies for
+each request.
+
+
+TODO
+====
+
+* add the OB connection pool support.
+
+Author
+======
+
+hugozhu <hugozhu@gmail.com>
+
+Copyright and License
+=====================
+
+This module is licensed under the BSD license.
+
+Copyright (C) 2012, by Yichun "agentzh" Zhang (章亦春) <agentzh@gmail.com>.
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+See Also
+========
+* the ngx_lua module: http://wiki.nginx.org/HttpLuaModule
+* the MySQL wired protocol specification: http://forge.mysql.com/wiki/MySQL_Internals_ClientServer_Protocol
+* the [lua-resty-memcached](https://github.com/agentzh/lua-resty-memcached) library
+* the [lua-resty-redis](https://github.com/agentzh/lua-resty-redis) library
+* the ngx_drizzle module: http://wiki.nginx.org/HttpDrizzleModule
+
---
+++ lib/resty/oceanbase.lua
@@ -0,0 +1,260 @@
+-- Copyright (C) 2012 Hugo Zhu (hugozhu)
+
+local bit = require "bit"
+local sub = string.sub
+local tcp = ngx.socket.tcp
+local insert = table.insert
+local strlen = string.len
+local strbyte = string.byte
+local strchar = string.char
+local strfind = string.find
+local strrep = string.rep
+local null = ngx.null
+local band = bit.band
+local bxor = bit.bxor
+local bor = bit.bor
+local lshift = bit.lshift
+local rshift = bit.rshift
+local tohex = bit.tohex
+local concat = table.concat
+local unpack = unpack
+local setmetatable = setmetatable
+local error = error
+local tonumber = tonumber
+local print = ngx.say
+local pairs = pairs
+
+module(...)
+
+_VERSION = '0.1'
+
+-- constants
+
+local mt = { __index = _M }
+
+
+local function _get_byte2(data, i)
+    local a, b = strbyte(data, i, i + 1)
+    return bor(a, lshift(b, 8)), i + 2
+end
+
+
+local function _get_byte3(data, i)
+    local a, b, c = strbyte(data, i, i + 2)
+    return bor(a, lshift(b, 8), lshift(c, 16)), i + 3
+end
+
+
+local function _get_byte4(data, i)
+    local a, b, c, d = strbyte(data, i, i + 3)
+    return bor(lshift(a, 24), lshift(b, 16), lshift(c, 8), d), i + 4
+end
+
+
+local function _get_byte8(data, i)
+    local a, b, c, d, e, f, g, h = strbyte(data, i, i + 7)
+    return bor(a, lshift(b, 8), lshift(c, 16), lshift(d, 24), lshift(e, 32),
+               lshift(f, 40), lshift(g, 48), lshift(h, 56)), i + 8
+end
+
+
+
+local function _get_cstring(data, i)
+    local last = strfind(data, "\0", i, true)
+    if not last then
+        return nil, nil
+    end
+
+    return sub(data, i, last - 1), last + 1
+end
+
+local function _get_int16(data,i)
+    local a, b = strbyte(data, i, i + 1)
+    return bor(lshift(a,8), b), i + 2
+end
+
+local function _get_int32(data,i)
+    local a, b, c, d = strbyte(data, i, i + 3)    
+    return bor(lshift(a,24), lshift(b,16), lshift(c,8), d), i+4
+end
+
+local function _set_byte2(n)
+    return strchar(band(rshift(n, 8), 0xff), band(n, 0xff))
+end
+
+local function _set_byte3(n)
+    return strchar(band(rshift(n, 16), 0xff),
+                   band(rshift(n, 8), 0xff), 
+                    band(n, 0xff))
+end
+
+local function _set_byte4(n)
+    return strchar(band(rshift(n, 24), 0xff), 
+        band(rshift(n, 16), 0xff), 
+        band(rshift(n, 8), 0xff),
+        band(n, 0xff)
+        )
+end
+
+
+local function _dump(data)
+    local bytes = {}
+    for i = 1, #data do
+        insert(bytes, strbyte(data, i, i))
+    end
+    return concat(bytes, " ")
+end
+
+
+local function _dumphex(data)
+    local bytes = {}
+    for i = 1, #data do
+        insert(bytes, tohex(strbyte(data, i), 2))
+    end
+    return concat(bytes, " ")
+end
+
+function _send_packet(self, req, size)
+    local sock = self.sock
+
+    self.packet_no = self.packet_no + 1
+
+    --print("packet no: ", self.packet_no)
+
+    local packet = {
+        _set_byte3(size),
+        strchar(self.packet_no),
+        req
+    }
+
+    --print("sending packet...")
+
+    return sock:send(packet)
+end
+
+local function _recv_packet(self)
+    local sock = self.sock
+    local msgType, msgLen, msg
+    local data, err = sock:receive(1)
+    msgType = data
+    data, err = sock:receive(4)
+    msgLen = _get_int32(data,1)
+    if msgLen - 4 > 0 then
+        data, err = sock:receive(msgLen - 4)
+        msg = data
+    end
+    return msgType, msg
+end
+
+function new(self)
+    local sock, err = tcp()
+    if not sock then
+        return nil, err
+    end
+    return setmetatable({ sock = sock }, mt)
+end
+
+function connect(self, opts)
+    local ok, err, data, length
+    local host = opts.host
+    local port = opts.port or 5432
+
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+
+    ok, err = sock:connect(host, port)
+    if not ok then
+        return nil, 'failed to connect: ' .. err
+    end
+
+    local params = {} -- oceanbase doesn't support authentication for now
+
+    length = 4 + 4
+    for k,v in pairs(params) do
+        length = length + k:len() + 1 + v:len() + 1
+    end
+    length = length + 1
+
+    sock:send({_set_byte4(length), _set_byte2(3), _set_byte2(0)})
+    for k,v in pairs(params) do
+        sock:send({k,"\0",v,"\0"})
+    end
+    sock:send({"\0"})
+
+    local typ, msg = _recv_packet(self)
+
+    repeat
+        local msgType, _ = _recv_packet(self)
+    until msgType == 'Z'  -- ready for query now
+
+    return typ == "R" and _get_int32(msg,1) == 0 
+end
+
+function query(self, sql, callback)
+    local sock = self.sock
+    local length = sql:len() + 4 + 1
+    sock:send({ "Q", _set_byte4(length), sql, "\0"})
+    local columns, pos, msgType, row, data
+    repeat
+        msgType, data = _recv_packet(self)
+
+        if msgType == 'T' then
+            columns, pos = _parse_row_description(data)
+            callback(columns)
+        end
+
+        if msgType == 'D' then
+            row, pos = _parse_row_data(data)
+            callback(row)
+        end
+
+    until msgType == 'C'
+end
+
+function _parse_row_data(data)
+    local num_of_field, pos = _get_int16(data, 1)
+    local columns = {}
+    local msgLen
+    for i=1, num_of_field do
+        msgLen, pos = _get_int32(data, pos)
+        local bytes = sub(data, pos, pos + msgLen - 1)
+        insert(columns, bytes)
+        pos = pos + msgLen
+    end
+    return columns, pos
+end
+
+function _parse_row_description(data)
+    local num_of_field, pos = _get_int16(data, 1)
+    local columns = {}
+    local column
+    for i=1, num_of_field do
+        column, pos = _get_cstring(data, pos)
+        pos = pos + ( 4 + 2 ) * 3
+        insert(columns, column)        
+    end
+    return columns, pos
+end
+
+function close(self)
+    local sock = self.sock
+    if not sock then
+        return nil, "not initialized"
+    end
+
+    self.state = nil
+
+    return sock:close()
+end
+
+local class_mt = {
+    -- to prevent use of casual module global variables
+    __newindex = function (table, key, val)
+        error('attempt to write to undeclared variable "' .. key .. '"')
+    end
+}
+
+setmetatable(_M, class_mt)
+
